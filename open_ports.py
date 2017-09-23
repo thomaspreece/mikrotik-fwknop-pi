d8d5aa9ac8e233fafcb43cf3ea0852c16d459797
Thomas Preece <thomaspreece10@gmail.com>
2017-09-23 14:45:50 +0000

    Minor fixes to open_ports python script

--- open_ports.py
+++ open_ports.py
@@ -5,10 +5,10 @@ parser = ArgumentParser(description='Add addresses to Firewall list on router. L
 parser.add_argument('--routerip', help='IP address of router', default="192.168.1.1")
 parser.add_argument('--routeruser', help='Username of API user on router', default="apiuser")
 parser.add_argument('--routerpass', help='Password of API user on router', default="abc123")
-parser.add_argument('clientip', help='IP address of client, i.e the IP to add to address list', required=True)
-parser.add_argument('protocol', help='Protocol, 6=tcp, 17=udp', required=True)
-parser.add_argument('port', help='Port', required=True)
-parser.add_argument('time', help='Length of time (in seconds) to keep address in list', required=True)
+parser.add_argument('clientip', help='IP address of client, i.e the IP to add to address list')
+parser.add_argument('protocol', help='Protocol, 6=tcp, 17=udp')
+parser.add_argument('port', help='Port')
+parser.add_argument('time', help='Length of time (in seconds) to keep address in list')
 args = parser.parse_args()
 
 
@@ -21,4 +21,4 @@ hours = (time/(60*60) % 24)
 mins = (time/60 % 60)
 secs = (time % 60)
 
-api(cmd='/ip/firewall/address-list/add', list="FWKNOP_{0}_{1}".format(args.protocol, args.port), address=args.clientip timeout="{0}d {1:02d}:{2:02d}:{3:02d}".format(days,hours,mins,secs))
+api(cmd='/ip/firewall/address-list/add', list="FWKNOP_{0}_{1}".format(args.protocol, args.port), address=args.clientip, timeout="{0}d {1:02d}:{2:02d}:{3:02d}".format(days,hours,mins,secs))
