e18ae3a255c277dd924ffef9ddfa96d29bf3b560
Thomas Preece <thomas.preece@bbc.com>
2019-06-09 17:47:53 +0100

    Added in email notifications

--- open_ports.py
+++ open_ports.py
@@ -5,6 +5,7 @@ parser = ArgumentParser(description='Add addresses to Firewall list on router. L
 parser.add_argument('--routerip', help='IP address of router', default="192.168.1.1")
 parser.add_argument('--routeruser', help='Username of API user on router', default="apiuser")
 parser.add_argument('--routerpass', help='Password of API user on router', default="abc123")
+parser.add_argument('--email', help='Email to send message to', default=None)
 parser.add_argument('clientip', help='IP address of client, i.e the IP to add to address list')
 parser.add_argument('protocol', help='Protocol, 6=tcp, 17=udp')
 parser.add_argument('port', help='Port')
@@ -22,3 +23,11 @@ mins = (time/60 % 60)
 secs = (time % 60)
 
 api(cmd='/ip/firewall/address-list/add', list="FWKNOP_{0}_{1}".format(args.protocol, args.port), address=args.clientip, timeout="{0}d {1:02d}:{2:02d}:{3:02d}".format(days,hours,mins,secs))
+
+if(args.email != None):
+    proto_string = ""
+    if(args.protocol == "6"):
+        proto_string = "tcp"
+    elif(args.protocol == "17"):
+        proto_string = "udp"
+    api(cmd='/tool/e-mail/send', to=args.email, subject='Router: Port Opened', body='Given IP: {4} access to {5}:{6} for {0}d {1:02d}:{2:02d}:{3:02d}'.format(days,hours,mins,secs, args.clientip, proto_string, args.port))
